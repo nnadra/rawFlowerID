0674365519707de4cddf7f6395c31a6220976723
ranchiika <ranichicka@gmail.com>
2025-05-13 10:50:00 +0700

    Nambahin kebutuhan flowbite, Jangan lupa install npm install flowbite

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -41,8 +41,8 @@ const Navbar = () => {
               <div className="flex gap-5">
                 <div className="h-10 w-10 rounded-full bg-amber-900"/>
                 <button 
-                  id="usernameHome"
-                  data-dropdown-toggle="usernameHome"
+                  id="dropdownButton"
+                  data-dropdown-toggle="dropdownMenu"
                   className="text-white bg-amber-800 hover:bg-amber-900 focus:ring-4 focus:outline-none focus:ring-amber-950 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center" type="button">
                   {userName}
                   <svg NameName="w-2.5 h-2.5 ms-3" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 10 6">
@@ -52,7 +52,7 @@ const Navbar = () => {
               </div>
 
             {/* Dropdown Menu */}
-            <div id="usernameHome" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
+            <div id="dropdownMenu" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-sm w-44 dark:bg-gray-700 dark:divide-gray-600">
               <div className="px-4 py-3 text-sm text-gray-900 dark:text-white">
                 <div>{userName}</div>
                 <div className="font-medium truncate">name@flowbite.com</div>
