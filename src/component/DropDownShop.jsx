dc21ee906c468af038af9c1ad46c7cefff5b1e46
KaylaSyahma <ksyahma22@gmail.com>
2025-05-25 09:03:06 +0700

    dropdown category done

--- src/component/DropDownShop.jsx
+++ src/component/DropDownShop.jsx
@@ -1,10 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axiosClient from "../utils/helper";
 
 export default function DropDownShop() {
   const [isOpen, setIsOpen] = useState(false);
   const [selected, setSelected] = useState("Choose a category");
-
-  const options = ["Popular", "Fresh Flower", "Dummy Flower", "Snack Bouquet"];
+  const [categories, setCategories] = useState([]);
+  
+  useEffect(() => {
+      axiosClient.get('/api/categories')
+        .then((response) => {
+        setCategories(response.data.data);  // Sesuaikan dengan struktur data dari backend
+        })
+        .catch((err) => console.error('Gagal fetch kategori:', err));
+    }, []);
+  // const options = ["Popular", "Fresh Flower", "Dummy Flower", "Snack Bouquet"];
 
   return (
     <div className="relative w-64">
@@ -17,16 +26,16 @@ export default function DropDownShop() {
 
       {isOpen && (
         <ul className="absolute z-10 mt-1 w-full bg-white border border-gray-300 rounded-md shadow-lg">
-          {options.map((option, index) => (
+          {categories.map((category, index) => (
             <li
               key={index}
               onClick={() => {
-                setSelected(option);
+                setSelected(category);
                 setIsOpen(false);
               }}
               className="px-4 py-2 hover:bg-[#4E2A1E] hover:text-[#FFF5E3] cursor-pointer"
             >
-              {option}
+              {category.name}
             </li>
           ))}
         </ul>
